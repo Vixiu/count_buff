0b8a1cb280c9793a4be3b266f671db8c83ff4597
Vixiu <93539736+Vixiu@users.noreply.github.com>
2023-09-10 20:15:02 +0800

    v1.1

--- 打包.bat
+++ 打包.bat
@@ -1,1 +1,1 @@
-nuitka --windows-icon-from-ico=F:\count_buff\png_wait\ah90u-1gfk2.ico --standalone --mingw64 --show-memory --show-progress --nofollow-imports --enable-plugin=pyqt5 --follow-import-to=need --output-dir=o --standalone  --windows-disable-console  --onefile  main.py
+nuitka --windows-icon-from-ico=F:\count_buff\uncompiled\png_notused\ah90u-1gfk2.ico --standalone --mingw64 --show-memory --show-progress --nofollow-imports --enable-plugin=pyqt5 --follow-import-to=need --output-dir=o --standalone  --windows-disable-console  --onefile  main.py
